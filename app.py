8180dc32a6f7d952707b4829641653412e5b96b8
EC2 Default User <ec2-user@ip-172-31-21-146.ec2.internal>
2020-10-11 18:29:55 +0000

    Bot fully functional

--- app.py
+++ app.py
@@ -4,11 +4,6 @@ from flask import request
 import flask_socketio
 import random
 import requests
-import emoji
-
-import random
-import requests
-import emoji
 
 
 app = flask.Flask(__name__)
@@ -18,15 +13,14 @@ num_users = 0
 new_username = ""
 
 
-def bot(input_message):
-    if (input_message == "!!about"):
-        server_socket.emit('message', {
-            "message": "This is the best chat app ever",
-            "new_username": "Bot"
-        })
-        
+def bot(message):
+    if (message["new_message"] == "!!about"):
+        print("Function entered !!about")
+        message['new_username'] = "Poke-bot"
+        message['new_message'] = "This is the best chat ever"
+        server_socket.emit('message', { 'new_username': new_username, "message": message } )
             
-    elif (input_message == "!!pokemon"):
+    elif (message["new_message"] == "!!pokemon"):
         poke_num = random.randint(1, 151)
         api_link = f"https://pokeapi.co/api/v2/pokemon/{poke_num}"
         poke_data = requests.get(api_link)
@@ -34,9 +28,12 @@ def bot(input_message):
         poke_name = pack_data['species']['name']
         poke_type = pack_data['types'][0]['type']['name']
         print(poke_name, poke_type)
-        server_socket.send(poke_name + " is a " + poke_type + " pokemon! :)")
+        
+        message['new_username'] = "Poke-bot"
+        message['new_message'] = poke_name + " is a " + poke_type + " pokemon! :)"
+        server_socket.emit('message', { 'new_username': new_username, "message": message } )
             
-    elif (input_message == "!!mood"):
+    elif (message["new_message"] == "!!mood"):
         moods = [ "I'm feeling happy.",
                 "I'm feeling sleepy.",
                 "I'm feeling awkward.",
@@ -47,21 +44,30 @@ def bot(input_message):
                 "I'm feeling lazy....."]
         server_socket.send(random.choice(moods))
             
-    elif (input_message == "!!help"):
-        server_socket.send("Select from one of these: !!about -  !!funtranslate [message] - !!mood -  !!pokemon - !!help")
+    elif (message["new_message"] == "!!help"):
+        message['new_username'] = "Poke-bot"
+        message['new_message'] = "Select from one of these: !!about -  !!funtranslate [message] - !!mood -  !!pokemon - !!help"
+        server_socket.emit('message', { 'new_username': new_username, "message": message } )
             
-    elif (input_message.split()[0] == "!!funtranslate"):
-        phrase= input_message[15:]
+    elif (message["new_message"].split()[0] == "!!funtranslate"):
+        phrase= message["new_message"][15:]
         """
         api_link = f"https://api.funtranslations.com/translate/doge.json?text={phrase}"
         doge_data = requests.get(api_link)
         pack_data = doge_data.json()
         translated = pack_data['contents']['translated']
+        message['new_message'] = translated
         """
-        server_socket.send("PLACEHOLDER VALUE - DOGE")
+        message['new_username'] = "Poke-bot"
+        message['new_message'] = "Placeholder"
+        
+        server_socket.emit('message', { 'new_username': new_username, "message": message } )
         
     else:
-        server_socket.send("Sorry, that's not a proper command. :/")
+        message['new_username'] = "Poke-bot"
+        message['new_message'] = "Sorry, that is not a valid command. :/"
+        
+        server_socket.emit('message', { 'new_username': new_username, "message": message } )
         
 
 @app.route('/')
@@ -101,17 +107,12 @@ def on_disconnect():
 @server_socket.on('message')
 def message_handler(message):
     global new_username
-    server_socket.emit('message', {
-        'message': message,
-        'new_username': new_username
-    })
-    print("Received message: ", message["new_username"], message["new_message"])
-    
+    server_socket.emit('message', { 'message': message, 'new_username': new_username })
     if message["new_message"][0:2] == "!!":
-        bot(message["new_message"])
-        
-    
+        bot(message)
+    print("Received message: ", message["new_username"], message["new_message"])
 
+        
 if __name__ == '__main__': 
     server_socket.run(
         app,
